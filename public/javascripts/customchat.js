ff39de4ca53a91ef49aa65897e201443c6825532
Jovin Mwilanga <jovinbeda@hotmail.com>
2014-12-28 17:23:49 -0500

    Fixed bug that only upvotes the first ten questions only

--- public/javascripts/customchat.js
+++ public/javascripts/customchat.js
@@ -34,7 +34,10 @@ $(document).ready(function () {
         });
 
         //send upvote event with the question index to server
-        socket.emit('upvote', upvoteId.substring(1, 3));
+        // first check if upvoteId has 4 chars(for classes like .a4b)
+        //5 chars(for classes like .a40b etc
+        var stringLimit = upvoteId.length;
+        socket.emit('upvote', upvoteId.substring(1, stringLimit));
     });
 
     $("table.topQuestions").delegate("button", "click", function () {
