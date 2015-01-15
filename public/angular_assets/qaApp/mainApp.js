ccb56422464496a6bb8287fa404e14fba9fd3977
Jovin Mwilanga <jovinbeda@hotmail.com>
2015-01-14 19:11:49 -0500

    Changed the questions display structure from table to bootstrap media

--- public/angular_assets/qaApp/mainApp.js
+++ public/angular_assets/qaApp/mainApp.js
@@ -1,7 +1,4 @@
 /**
  * Created by jovinbm on 1/12/15.
  */
-angular.module('qaApp', ['angular-loading-bar', 'textAngular', 'ngSanitize'])
-    .config(['cfpLoadingBarProvider', function(cfpLoadingBarProvider) {
-        cfpLoadingBarProvider.latencyThreshold = 5;
-    }]);
+angular.module('qaApp', ['textAngular', 'ngSanitize']);
