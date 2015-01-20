c0200d3e945465fcc1ad6c220946d06042efcab4
Jovin Mwilanga <jovinbeda@hotmail.com>
2015-01-20 18:23:50 -0500

    Fixed wrapping bug which caused question to overshoot their boxes. Also removed media style to impement ul style

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,6 +2,10 @@
  * Created by jovinbm on 1/13/15.
  */
 $(document).ready(function(){
+
+    //using bootstrap validator for form validation
+    $("#question_input").bootstrapValidator();
+
     $('.pull-down').each(function() {
         $(this).css('margin-top', $(this).parent().height()-$(this).height())
     });
