29292a10ce0b41ca932eb19d51854a068059931c
Jovin Mwilanga <jovinbeda@hotmail.com>
2015-01-21 09:49:37 -0500

    Added bootstrap validarion, plus error messages on both question and edit inputs

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,12 +1,5 @@
 /**
  * Created by jovinbm on 1/13/15.
  */
-$(document).ready(function(){
-
-    //using bootstrap validator for form validation
-    $("#question_input").bootstrapValidator();
-
-    $('.pull-down').each(function() {
-        $(this).css('margin-top', $(this).parent().height()-$(this).height())
-    });
+$(document).ready(function () {
 });
