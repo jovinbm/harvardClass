cb6e707a9034362290c0bcc46912dd6a873b825b
Jovin Mwilanga <jovinbeda@hotmail.com>
2015-01-22 02:06:01 -0500

    modified and updated error pages

--- todo.js
+++ todo.js
@@ -1,23 +1,31 @@
 /**
  * Created by jovinbm on 1/1/15.
  */
-
-//TODO -- force the ajax requests that are have failed queries to sign out and correct problem
-
-//TODO -- where you only want to check for 'err' and where you want to check for exactly 'err || theUser == null || theUser == undefined'
-
 //TODO -- app.js -- redirect unverified requests
 
 //TODO -- find and findOne -- try to optimize
 
-//TODO -- socket.on disconnect?
-
 //todo -- custom code?
-
-//todo -- filter to remove whitespace from users input, because when it is formatted, it brings overlapping problems
 
 //todo -- add google analytics to site
 
 //todo - slide on left for xs grid on chat to see top voted and online
 
 //todo -- return parts of upvoted and promoted to improve performance
+
+//todo -- edit comments
+
+//todo -- delete a question
+
+//todo -- delete a comment
+
+//todo -- remove upvote
+
+//todo -- remove promote
+
+//todo -- search
+
+//todo -- alert new comment or question available
+
+//todo -- angular error handling -- flash messages(alerts) by bootstrap
+
